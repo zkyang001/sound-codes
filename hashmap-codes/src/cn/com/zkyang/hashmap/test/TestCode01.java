1f6facce1b218b0ad6e3c817a6606856364b2d11
zkyang <zkyang@ccxi.com.cn>
2020-07-28 11:22:55 +0800

    2020-07-28 hashmap源码第一讲

---
+++ hashmap-codes/src/cn/com/zkyang/hashmap/test/TestCode01.java
@@ -0,0 +1,12 @@
+package cn.com.zkyang.hashmap.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestCode01 {
+
+    public static void main(String[] args) {
+        Map map = new HashMap<>();
+
+    }
+}
